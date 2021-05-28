bb2b82f206b9b3a59daa9137206ed3c0a08826be
Theo <theomhalpern@gmail.com>
2021-05-28 15:12:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,32 +6,39 @@ import aiohttp
 import asyncio
 import pyppeteer
 import discord
+import logging
+import math
 
 workingDir = os.getcwd()
 
 downloadCount = 0
 
 async def run_command(video_full_path, output_file_name, target_size):
-	cmd = f'ffmpeg -i {video_full_path} -preset veryfast -vcodec libx264 -crf 28 -c:a: aac {output_file_name}'
+	pro1 = await asyncio.create_subprocess_exec(*["ffprobe", "-v", "error", "-show_entries","format=duration", "-of","default=noprint_wrappers=1:nokey=1", video_full_path], stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+	print("Started: %s, pid=%s" % (pro1, pro1.pid), flush=True)
+	stdout, stderr = await pro1.communicate()
+	print(float(stdout))
+	bitrate = str(math.floor(8*8100/float(stdout))-32)+"k"
+	print(bitrate)
 
-	# Create subprocess
-	process = await asyncio.create_subprocess_exec(
-		*cmd.split(), stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+	# consider making very fast not ultrafast
+	cmd =f"ffmpeg -y -i {video_full_path} -c:v libx264 -passlogfile {video_full_path}passlog -preset ultrafast -b:v {bitrate} -pass 1 -an -f mp4 {output_file_name}"
+	print(cmd)
+
+	cmd2 =f"ffmpeg -y -i {video_full_path} -c:v libx264 -passlogfile {video_full_path}passlog -preset ultrafast -b:v {bitrate} -pass 2 -acodec copy {output_file_name}"
+	print(cmd2)
+
+	pro1 = await asyncio.create_subprocess_exec(*cmd.split(), stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
 	)
-
-	# Status
-	print("Started: %s, pid=%s" % (process, process.pid), flush=True)
-
-	# Wait for the subprocess to finish
-	stdout, stderr = await process.communicate()
+	print("Started: %s, pid=%s" % (pro1, pro1.pid), flush=True)
+	stdout, stderr = await pro1.communicate()
 	
-	# Progress
-	if process.returncode == 0:
-		print("Done compression")
-	else:
-		print("Failed compression")
-		print(stderr)
+	pro1 = await asyncio.create_subprocess_exec(*cmd2.split(), stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+	)
+	print("Started: %s, pid=%s" % (pro1, pro1.pid), flush=True)
+	stdout, stderr = await pro1.communicate()
 
+	os.remove(video_full_path +"passlog-0.log")
 	print(os.path.getsize(output_file_name))
 	os.remove(video_full_path)
 
@@ -50,16 +57,16 @@ async def downloadTiktok(url):
 		});
 		page = await browser.newPage()
 		await page.goto(url)
-		
 		try:
 			element = await page.querySelector('video')
 			videoUrl = await page.evaluate('(element) => element.src', element)
 		except:
 			await page.goto(url)
 			# await page.waitForSelector('video')
-			# await page.screenshot({'path': 'example.png'})
 			element = await page.querySelector('video')
 			videoUrl = await page.evaluate('(element) => element.src', element)
+
+		# await page.screenshot({'path': 'example.png'})
 		
 		print(videoUrl)
 		cookies = await page.cookies()
@@ -90,9 +97,9 @@ async def downloadTiktok(url):
 		print(os.path.getsize(pathToLastFile));
 
 		#sends the file off to compression if it is over 8 mb
-		if(os.path.getsize(pathToLastFile) >= 8000000):
+		if(os.path.getsize(pathToLastFile) >= 8388000):
 			print("compressing ...")
-			await run_command(pathToLastFile, pathToLastFile + "comp.mp4", 8000000)
+			await run_command(pathToLastFile, pathToLastFile + "comp.mp4", 8388000)
 			pathToLastFile = pathToLastFile + "comp.mp4"
 		
 		#returns the path to the file
@@ -105,7 +112,6 @@ async def downloadTiktok(url):
 		except:
 			pass
 
-import logging
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
@@ -118,51 +124,60 @@ keep_alive()
 # sets the activity to listing to &help one start
 client = discord.Client(activity=discord.Activity(type=discord.ActivityType.listening, name='&Help'))
 
+def getGuildName(n):
+  return [n.name, n.member_count]
+
 @client.event
 async def on_ready():
-		print('Logged in as')
-		print(client.user.name)
-		print(client.user.id)
-		print(len(client.guilds))
-		print('------')
+	print('Logged in as')
+	print(client.user.name)
+	print(client.user.id)
+	print(list(map(getGuildName, client.guilds)))
+	print(len(client.guilds))
+	print('------')
 
 @client.event
 async def on_message(message):
-		# we do not want the bot to reply to itself
-		if message.author.id == client.user.id:
-			return
-		
-		# sends a description of the bot on &help
-		elif message.content.lower().startswith('&help'):
-			embed=discord.Embed(title="About TikTok Auto Embed", description="When you post a tiktok link Tiktok Auto Embed will delete your original message and send a message containing the original link, the senders @, and a dowloaded copy of the video.", color=0xFF5733)
-			embed.add_field(name="Why do long videos look bad?", value="Discord has a file limit of 8 mb, this isn't a problem for most tiktoks but for some the bot needs to compress them before sending. Because of this it can also take the bot longer to send long vides espeically if a lot of people are using the bot at once.", inline=False)
-			embed.add_field(name="Say Hi to the Creator", value="Message me <@!322193320199716865> and join my discord server dedicated to my projects [https://discord.gg/fKcTKxW6Jv](https://discord.gg/fKcTKxW6Jv).", inline=False)
-			await message.channel.send(embed=embed)
-		
-		# tries to download if it sees .tiktok. in a message
-		elif (re.search("\.tiktok\.", message.content) != None):
-			toEdit = await message.channel.send('working on it', mention_author=True)
+	# we do not want the bot to reply to itself
+	if message.author.id == client.user.id:
+		return
+	
+	# sends a description of the bot on &help
+	elif message.content.lower().startswith('&help'):
+		embed=discord.Embed(title="About TikTok Auto Embed", description="When you post a tiktok link Tiktok Auto Embed will delete your original message and send a message containing the original link, the senders @, and a dowloaded copy of the video.", color=0xFF5733)
+		embed.add_field(name="Can I add this bot to my server?", value="Yep, just go to this link.\nhttps://tinyurl.com/TiktokAutoEmbed", inline=False)
+		embed.add_field(name="Why do long videos look bad?", value="Discord has a file limit of 8 mb, this isn't a problem for most tiktoks but for some the bot needs to compress them before sending. Because of this it can also take the bot longer to send long vides espeically if a lot of people are using the bot at once.", inline=False)
+		embed.add_field(name="Say Hi to the Creator", value="Message me <@!322193320199716865> and join my discord server dedicated to my projects [https://discord.gg/fKcTKxW6Jv](https://discord.gg/fKcTKxW6Jv).", inline=False)
+		await message.channel.send(embed=embed)
+	
+	# tries to download if it sees .tiktok. in a message
+	elif (re.search("\.tiktok\.", message.content) != None):
+		toEdit = await message.channel.send('working on it', mention_author=True)
+		try:
+			fileLoc = await downloadTiktok(message.content)
+			print(message.guild)
+			file = discord.File(fileLoc)
+			embed=discord.Embed(url=message.content, description=message.content, color=discord.Color.blue())
+			# uses the authors nick name if they have one
 			try:
-				fileLoc = await downloadTiktok(message.content)
-				print(message.guild)
-				file = discord.File(fileLoc)
-				embed=discord.Embed(url=message.content, description=message.content, color=discord.Color.blue())
-				# uses the authors nick name if they have one
 				if(message.author.nick != None):
 					embed.set_author(name=message.author.nick, url="https://discordapp.com/users/"+str(message.author.id), icon_url=message.author.avatar_url)
 				else:
-					embed.set_author(name=message.author, url="https://discordapp.com/users/"+str(message.author.id), icon_url=message.author.avatar_url)	
-				await message.channel.send(embed=embed,file=file)
-				print(fileLoc)
-				os.remove(fileLoc)
-				await toEdit.delete()
-				#tries to delete the user sent message 
-				try:
-					await message.delete()
-				except:
-					print("no perms")
-			except Exception as e: 
-				print(e)
-				await toEdit.delete()
+					embed.set_author(name=message.author, url="https://discordapp.com/users/"+str(message.author.id), icon_url=message.author.avatar_url)
+			except:
+				print("pm")
+				embed.set_author(name=message.author, url="https://discordapp.com/users/"+str(message.author.id), icon_url=message.author.avatar_url)
+			await message.channel.send(embed=embed,file=file)
+			print(fileLoc)
+			os.remove(fileLoc)
+			await toEdit.delete()
+			#tries to delete the user sent message 
+			try:
+				await message.delete()
+			except:
+				print("no perms")
+		except Exception as e: 
+			print(e)
+			await toEdit.delete()
 
 client.run(my_secret)
